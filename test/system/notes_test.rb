c374793e733f89c96a951323600d04347ca1738c
hkmjtech <ajmlhkm@gmail.com>
2023-05-25 21:36:45 +0800

    swap sass with tailwind and add notes

--- test/system/notes_test.rb
+++ test/system/notes_test.rb
@@ -1,6 +1,10 @@
 require "application_system_test_case"
 
 class NotesTest < ApplicationSystemTestCase
+  setup do
+    @note = notes(:first) # Reference to the first fixture quote
+  end
+
   test "Creating a new note" do
     # When we visit the Notes#index page
     # we expect to see a title with the text "Notes"
@@ -22,4 +26,34 @@ class NotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Notes"
     assert_text "Capybara note"
   end
+  
+  test "Showing a note" do
+    visit notes_path
+    click_link @note.name
+
+    assert_selector "h1", text: @note.name
+  end
+
+  test "Updating a note" do
+    visit notes_path
+    assert_selector "h1", text: "Notes"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit note"
+
+    fill_in "Name", with: "Updated note"
+    click_on "Update note"
+
+    assert_selector "h1", text: "Notes"
+    assert_text "Updated note"
+  end
+
+  test "Destroying a note" do
+    visit notes_path
+    assert_text @note.name
+
+    click_on "Delete", match: :first
+    assert_no_text @note.name
+  end
+
 end
